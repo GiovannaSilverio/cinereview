7bec5f706fdfa228c6474274bdc5430c1ee8ed30
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2024-07-03 11:03:26 -0300

    feat: adiciona filtro de rotas por meio do cookie.

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,60 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { useRouter } from "next/navigation";
+import api from "@/services/api";
+
+interface RootObject {
+  page: number;
+  results: Result[];
+  total_pages: number;
+  total_results: number;
+}
+interface Result {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    async function getMovies() {
+      try {
+        const response = await api.get("/movies/page/2", {
+          withCredentials: true, // para enviar o cookie
+        });
+        console.log(response.status);
+
+      } catch (error: any) {
+        if (error.response && error.response.status === 403) {
+          console.error("Usuário não autenticado");
+          router.push("/login");
+   
+        } else {
+          console.error("Erro na requisição:", error);
+        }
+      }
+    }
+
+    getMovies();
+  }, []);
+
   return (
     <>
       <Header />
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,23 +1,96 @@
+"use client";
 import React from "react";
+import api from "@/services/api";
 import Image from "next/image";
-import Link from "next/link"
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+// react hook form
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+// yup
+import { object, string } from "yup";
+
+const schema = object({
+  email: string().required("campo de email obrigatório"),
+  password: string()
+    .required("campo de senha obrigatório")
+    .min(4, "Sua senha deve ter pelo menos 4 digitos"),
+});
 
 export default function Login() {
-    return(
-        <div className="bg-black flex items-center justify-center font-jura h-screen w-screen">
-            <section className=" bg-gray flex flex-col w-2/5 px-8 py-12 rounded-lg items-center gap-3">
-                <Image alt="logo do cinereview" width={215} height={135} src="/img/logo.png"/>
-                <label className="self-start font-heavitas text-white" htmlFor="">E-MAIL</label>
-                <input placeholder="Digite seu e-mail" className="placeholder-black placeholder-opacity-50 bg-lightgray pl-4 w-full py-2 rounded-md text-white self-start" type="email" />
-                <label className="self-start font-heavitas text-white" htmlFor="">SENHA</label>
-                <input placeholder="Digite sua senha" className="placeholder-black placeholder-opacity-50 bg-lightgray pl-4 w-full py-2 rounded-md text-white self-start" type="password" />
-                <div className="flex flex-row self-start w-full justify-between">
-                    <p className="self-end text-white">Não possui login? <span className="text-yellow">
-                    <Link href="/cadastro">Cadastre-se</Link></span></p>
-                    <button 
-                    className="bg-red hover:bg-redhover bg-red2 font-heavitas text-white px-5 w-fit py-2 rounded-md">ENTRAR</button>
-                </div>
-            </section>                    
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+  const router = useRouter();
+
+  const onSubmit = async (data: any) => {
+    try {
+      const response = await api.post("/user/login", data, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      });
+      if (response.status === 200) {
+        alert("Login realizado com sucesso!");
+        router.push("/home");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Erro ao realizar login. Verifique suas credenciais e tente novamente.");
+    }
+  };
+
+  return (
+    <div className="bg-black flex items-center justify-center font-jura h-screen w-screen">
+      <section className="bg-gray flex flex-col w-2/5 px-8 py-12 rounded-lg items-center gap-3">
+        <Image alt="logo do cinereview" width={215} height={135} src="/img/logo.png" />
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-full gap-3">
+          <label className="self-start font-heavitas text-white" htmlFor="email">
+            E-MAIL
+          </label>
+          <input
+            {...register("email")}
+            id="email"
+            placeholder="Digite seu e-mail"
+            className="placeholder-black placeholder-opacity-50 bg-lightgray pl-4 w-full py-2 rounded-md text-white self-start"
+            type="email"
+          />
+          <span className="text-white self-start">{errors?.email?.message}</span>
+          
+          <label className="self-start font-heavitas text-white" htmlFor="password">
+            SENHA
+          </label>
+          <input
+            {...register("password")}
+            id="password"
+            placeholder="Digite sua senha"
+            className="placeholder-black placeholder-opacity-50 bg-lightgray pl-4 w-full py-2 rounded-md text-white self-start"
+            type="password"
+          />
+          <span className="text-white self-start">{errors?.password?.message}</span>
+
+          <div className="flex flex-row self-start w-full justify-between items-center mt-4">
+            <p className="self-end text-white">
+              Não possui login?{" "}
+              <span className="text-yellow">
+                <Link href="/cadastro">Cadastre-se</Link>
+              </span>
+            </p>
+            <button
+              type="submit"
+              className="bg-red hover:bg-redhover font-heavitas text-white px-5 w-fit py-2 rounded-md"
+            >
+              ENTRAR
+            </button>
+          </div>
+        </form>
+      </section>
     </div>
-    );
-};
+  );
+}
