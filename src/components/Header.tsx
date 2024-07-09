19b2b61db6e11407d55b7b5f7fd12f8133b70785
Giovanna <giosilverio.10@gmail.com>
2024-07-08 22:40:59 -0300

    adiciona logout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,18 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 
 export default function Header() {
     const [telaAtiva, setTelaAtiva] = useState("home");
+    const router = useRouter();
+
+    const logout = () => {
+        router.push("/login")
+        window.localStorage.removeItem("accessToken")
+        window.localStorage.removeItem("userId")
+    }
 
     return(
         <header className="flex bg-gray px-16 py-5 items-center font-jura font-bold text-white justify-between">
@@ -22,6 +30,13 @@ export default function Header() {
                 <Link 
                     className={`${ telaAtiva === "perfil" ? "text-yellow" : "text-white" }`}
                     href="/perfil" onClick={()=> setTelaAtiva("perfil")}>Perfil</Link>
+                <Link 
+                    className={`${ telaAtiva === "sair" ? "text-yellow" : "text-white" }`} 
+                    href="/login" 
+                    onClick={() => {logout(); setTelaAtiva("sair")}}
+                >
+                    Sair
+                </Link>
             </div>
         </header>
     );
